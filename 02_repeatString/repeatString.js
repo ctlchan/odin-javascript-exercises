5a941696590e4168b377e96ae55c324540e8ede2
Chris Chan <christakleungchan@gmail.com>
2023-06-07 19:41:09 -0400

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let result = "";
 
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            result += word;
+        }
+    }
+
+    else
+        result = "ERROR";
+
+    return result;
 };
 
 // Do not edit below this line
