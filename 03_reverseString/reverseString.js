eee8e7b07cf42a69709c2e7b9fe5d9b426d1ebb8
Chris Chan <christakleungchan@gmail.com>
2023-06-07 19:46:47 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let chars = string.split("");
+    let result = "";
 
+    for (let i = string.length - 1; i > -1; i--)
+        result += chars[i];
+
+    return result;
 };
 
 // Do not edit below this line
