2459bffde7c1e8e781c5d3af449b146554ac302f
Chris Chan <christakleungchan@gmail.com>
2023-06-07 20:18:36 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum = 0;
 
+    if (first < 0 || second < 0)
+        sum = "ERROR";
+
+    else if (typeof first != 'number' || typeof second != 'number')
+        sum = "ERROR";
+    
+    else {
+        let start = (first < second) ? first:second;
+        let end = (first > second) ? first:second;
+
+        for (let i = start; i <= end; i++)
+            sum += i;
+
+    }
+
+    
+
+    return sum;
 };
 
 // Do not edit below this line
