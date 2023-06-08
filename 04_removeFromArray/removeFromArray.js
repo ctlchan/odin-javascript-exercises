2e78566bfd010927c2a87b07eb9633b874d4f293
Chris Chan <christakleungchan@gmail.com>
2023-06-07 20:06:15 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...args) {
+    let result = [];
 
+    // forEach applies a function to each element in the array
+    list.forEach(
+
+        // we define the arrow function within the call to forEach
+        (item) => {
+            if (!args.includes(item))
+                result.push(item);
+        }
+
+    );
+
+    return result;
 };
 
 // Do not edit below this line
