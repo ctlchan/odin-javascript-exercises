639845554ee1c636b2bf2c72183487615354eaa8
Chris Chan <christakleungchan@gmail.com>
2023-07-01 12:50:59 -0400

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let len = word.length - 1;
+    let i = 0;
 
+    let result = true;
+
+    while (i < len && result) {
+
+        // TODO: fix while-loops so that it looks like: !(word[i].toUpper <= "A" && ... >= "Z")
+
+        while (!(word[i].toUpperCase() >= "A" && word[i].toUpperCase() <= "Z"))
+            i++;
+
+        while (!(word[len].toUpperCase() >= "A" && word[len].toUpperCase() <= "Z"))
+            len--;
+        
+        if (word[i].toLowerCase() != word[len].toLowerCase())
+            result = false;
+        
+        i++;
+        len--;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
