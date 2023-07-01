150ab324f177fb0fb62d134eb9add2a73cc56d85
Chris Chan <christakleungchan@gmail.com>
2023-07-01 13:16:59 -0400

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+    return people.reduce( (oldest, current) => {
+
+        let oldestLatestYear;
+        let currentLatestYear;
+
+        if (oldest.yearOfDeath === undefined)
+            oldestLatestYear = new Date().getFullYear();
+
+        else
+            oldestLatestYear = oldest.yearOfDeath;
+
+        if (current.yearOfDeath === undefined)
+            currentLatestYear = new Date().getFullYear();
+
+        else
+            currentLatestYear = current.yearOfDeath;
+
+
+        let oldestAge = oldestLatestYear - oldest.yearOfBirth;
+        let currentAge = currentLatestYear - current.yearOfBirth;
+
+        return (currentAge > oldestAge) ? current : oldest;
+    } );
 
 };
 
